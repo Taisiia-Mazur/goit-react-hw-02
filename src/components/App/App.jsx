9f7005f1f14d4f493e90815c5ae81e8fbe78488f
Taisiia <159428493+Taisiia-Mazur@users.noreply.github.com>
2024-08-16 17:50:10 +0300

    add homework

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,55 @@
-import Profile from "../Profile/Profile";
-import FriendList from "../FriendList/FriendList";
-import TransactionHistory from "../TransactionHistory/TransactionHistory";
+import { useState, useEffect } from "react";
 
-import userData from "../../userData.json";
-import friends from "../../friends.json";
-import transactions from "../../transactions.json";
-
-import css from "./App.module.css";
+import Description from "../Description/Description";
+import Notification from "../Notification/Notification";
+import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
 
 export default function App() {
+  const [value, setValue] = useState(() => {
+    const savedValues = window.localStorage.getItem("saved-value");
+    return savedValues
+      ? JSON.parse(savedValues)
+      : {
+          good: 0,
+          neutral: 0,
+          bad: 0,
+        };
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-value", JSON.stringify(value));
+  }, [value]);
+
+  const updateFeedback = (feedbackType) => {
+    setValue({ ...value, [feedbackType]: value[feedbackType] + 1 });
+  };
+
+  const resetValue = () => setValue({ good: 0, neutral: 0, bad: 0 });
+
+  const totalFeedback = value.good + value.neutral + value.bad;
+  const persent = Math.round((value.good / totalFeedback) * 100);
+
   return (
     <>
-      <Profile
-        avatar={userData.avatar}
-        username={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        stats={userData.stats}
-      />
+      <Description />
 
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <Options
+        click={updateFeedback}
+        feedback={Object.keys(value)}
+        totalValue={totalFeedback}
+        reset={resetValue}
+      />
+      {totalFeedback === 0 ? (
+        <Notification />
+      ) : (
+        <Feedback
+          value={value}
+          feedback={Object.keys(value)}
+          totalValue={totalFeedback}
+          persent={persent}
+        />
+      )}
     </>
   );
 }
